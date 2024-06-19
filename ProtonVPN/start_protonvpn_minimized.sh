9c6df02fa1df9663ba25bc890fafff3283347212
Àdhamh <84457316+MacBeibhinn@users.noreply.github.com>
2024-06-19 21:50:31 +0100

    Create start_protonvpn_minimized.sh

---
+++ ProtonVPN/start_protonvpn_minimized.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Start ProtonVPN
+protonvpn-app &
+# Function to check VPN connection status
+check_vpn_connection() {
+    nmcli -t -f TYPE connection show --active | grep "vpn"
+    return $?
+}
+# Loop to wait until VPN connection is established
+while :; do
+    if check_vpn_connection; then
+        # VPN is connected, find and hide ProtonVPN window
+        sleep 5  # Add a small delay to ensure VPN connection stability
+        WINDOW_ID=$(xdotool search --name "Proton VPN")
+        if [ -n "$WINDOW_ID" ]; then
+            # Hide the window and exit
+            xdotool windowunmap "$WINDOW_ID"
+            exit 0
+        fi
+    fi
+    sleep 2
+done
