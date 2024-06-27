035fd960a4c78537243b0e30bee6718e64e9f487
Àdhamh <84457316+MacBeibhinn@users.noreply.github.com>
2024-06-27 16:17:41 +0100

    Update open_background_link.sh

--- YouTube/open_background_link.sh
+++ YouTube/open_background_link.sh
@@ -2,7 +2,7 @@
 
 # Open Firefox with the specified URL in a new tab without waiting for it to launch
 firefox --new-tab "https://www.youtube.com" &
-# Use wmctrl to get the ID of the most recently opened window
+# Use xdotool to get the ID of the most recently opened window
 while [ -z "$WINDOW_ID" ]; do
     WINDOW_ID=$(xdotool search --onlyvisible --name "Mozilla Firefox" | tail -1)
     sleep 0.1
