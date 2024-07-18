2af66b16563c103b33cc9b203fc92a13f14fa694
Àdhamh <84457316+MacBeibhinn@users.noreply.github.com>
2024-07-18 16:47:30 +0100

    Create create_folders.sh

---
+++ System/create_folders.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Create folders and have them put on (in) the Desktop
+mkdir ~/Desktop/Folder1
+mkdir ~/Desktop/Folder2
+mkdir ~/Desktop/Folder3
+
+echo "Folders created on the desktop."
