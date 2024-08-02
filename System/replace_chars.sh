3c730c2c6aab7b7a9b0a69995cf3a5cf108a0782
Àdhamh <84457316+MacBeibhinn@users.noreply.github.com>
2024-08-02 16:12:16 +0100

    Update replace_chars.sh

--- System/replace_chars.sh
+++ System/replace_chars.sh
@@ -2,14 +2,14 @@
 
 # Function to replace characters, add more if needed (example: 's/oldChar/newChar/g', semicolon to break between)
 replace() {
-    echo $@ | sed 's/á/a/g; s/é/e/g'
+    echo $@ | sed 's/á/a/g; s/é/e/g; s/ø/o/g'
 }
 # Main script
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <text>"
     exit 1
 fi
-text=$1
+text=${1,,} # Replace "${1,,}" with "$1" to show uppercase in the output/echo; uppercase characters will then need to be defined separately in replace() or they will not be replaced
 replace_text=$(replace $text)
 echo "Modified text: $replace_text"
 # Run in the Terminal, ensure the file can be executed as a program:
