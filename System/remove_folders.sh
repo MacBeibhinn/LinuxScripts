2ba6e7a27b4fe9bd39ef619b2b3b2ee9a5c28aa5
Àdhamh <84457316+MacBeibhinn@users.noreply.github.com>
2024-08-04 19:02:33 +0100

    Update remove_folders.sh

--- System/remove_folders.sh
+++ System/remove_folders.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Remove folders from the Desktop
-rm -f ~/Desktop/Folder1
-rm -f ~/Desktop/Folder2
-rm -f ~/Desktop/Folder3
+rm -rf ~/Desktop/Folder1
+rm -rf ~/Desktop/Folder2
+rm -rf ~/Desktop/Folder3
 
 echo "Folders removed from the desktop."
