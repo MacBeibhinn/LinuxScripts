5ca853f31930af87b0677d41d60495e0367e1d21
Àdhamh <84457316+MacBeibhinn@users.noreply.github.com>
2024-10-18 20:13:30 +0100

    Update clear_chrome_data.sh

--- Chrome/clear_chrome_data.sh
+++ Chrome/clear_chrome_data.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Clear Chrome history, recent tabs, and cache:
+# Clear Chrome history, sessions, visited links, top sites, and cache
 rm -rf ~/.config/google-chrome/Default/History
 rm -rf ~/.config/google-chrome/Default/Sessions
+rm -rf ~/.config/google-chrome/Default/Visited\ Links
+rm -rf ~/.config/google-chrome/Default/Top\ Sites
 rm -rf ~/.config/google-chrome/Default/Cache/*
 # Clear download history, only works if sqlite3 is installed, remove/comment the line if you do not want this:
 sqlite3 ~/.config/google-chrome/Default/History "DELETE FROM downloads;"
